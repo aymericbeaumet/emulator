2d06a487ec54964f757246022ec9c00ee1e09179
Aymeric Beaumet <hi@aymericbeaumet.com>
2018-04-16 01:59:44 +0200

    fix unused variable

--- packages/shared/src/emulators/game_boy_color/processor/dec.rs
+++ packages/shared/src/emulators/game_boy_color/processor/dec.rs
@@ -2,7 +2,7 @@ use super::super::registers::Flag;
 use super::{instructions::*, operands::*, Cycle, Instruction, MemoryMap, Processor, Registers};
 
 impl Instruction<DEC, C, Void> for Processor {
-  fn instruction(r: &mut Registers, mm: &mut MemoryMap, _: C, _: Void) -> Cycle {
+  fn instruction(r: &mut Registers, _: &mut MemoryMap, _: C, _: Void) -> Cycle {
     let c = r.get_c() - 1;
     r.set_c(c);
     r.set_flag(Flag::Z, c != 0);
